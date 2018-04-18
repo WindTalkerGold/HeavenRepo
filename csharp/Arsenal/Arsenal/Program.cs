782aa9c2bd773817939f917a44ecb8da4bcbd2bd
WangXI <867110907@qq.com>
2018-04-18 23:18:03 +0800

    add concurrent reader lib

---
+++ csharp/Arsenal/Arsenal/Program.cs
@@ -0,0 +1,20 @@
+﻿using MultiThread;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+namespace Arsenal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentQueue<int> length = new ConcurrentQueue<int>();
+            ConcurrentFileReader reader = new ConcurrentFileReader(@"D:\Wang\test.txt", str => { length.Enqueue(str.Length); });
+            System.Console.WriteLine(reader.ReadLines());
+
+            foreach (int l in length)
+            {
+                System.Console.WriteLine(l);
+            }
+        }
+    }
+}
