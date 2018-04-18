734711ba3e97a09b185e60fa7a0355b92211643a
WangXI <867110907@qq.com>
2018-04-18 22:21:58 +0800

    add the solution for 139WordBreak, with dp

---
+++ leetcode/139WordBreak.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	bool wordBreak(string s, vector<string>& wordDict) {
+		unordered_set<string> dict(wordDict.begin(), wordDict.end());
+		vector<bool> dp(s.size() + 1, false);
+		dp[0] = true;
+
+		for (int i = 1; i <= s.size(); i++) {
+			for (int j = 1; j <= i; j++) {
+				if (!dp[i - j])
+					continue;
+				string word = s.substr(i - j, j);
+				if (dict.count(word)) {
+					dp[i] = true;
+					break;
+				}
+			}
+		}
+
+		return *dp.rbegin();
+	}
+
+};
