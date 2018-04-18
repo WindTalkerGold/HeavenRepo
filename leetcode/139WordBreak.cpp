60aa85bf4301175d4c225a5f23f7fb2e1de27891
WangXI <867110907@qq.com>
2018-04-18 22:27:12 +0800

    use whitespaces instead of tabs

--- leetcode/139WordBreak.cpp
+++ leetcode/139WordBreak.cpp
@@ -5,24 +5,24 @@ using namespace std;
 
 class Solution {
 public:
-	bool wordBreak(string s, vector<string>& wordDict) {
-		unordered_set<string> dict(wordDict.begin(), wordDict.end());
-		vector<bool> dp(s.size() + 1, false);
-		dp[0] = true;
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
 
-		for (int i = 1; i <= s.size(); i++) {
-			for (int j = 1; j <= i; j++) {
-				if (!dp[i - j])
-					continue;
-				string word = s.substr(i - j, j);
-				if (dict.count(word)) {
-					dp[i] = true;
-					break;
-				}
-			}
-		}
+        for (int i = 1; i <= s.size(); i++) {
+            for (int j = 1; j <= i; j++) {
+                if (!dp[i - j])
+                    continue;
+                string word = s.substr(i - j, j);
+                if (dict.count(word)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
 
-		return *dp.rbegin();
-	}
+        return *dp.rbegin();
+    }
 
 };
--- leetcode/623SmallestRange.cpp
+++ leetcode/623SmallestRange.cpp
@@ -6,45 +6,45 @@ using namespace std;
 
 class Solution {
 public:
-	vector<int> smallestRange(vector<vector<int>>& nums) {
-		vector<int> result;
-		if (nums.size() == 1) {
-			result.push_back(nums[0][0]);
-			result.push_back(nums[0][0]);
-			return result;
-		}
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        vector<int> result;
+        if (nums.size() == 1) {
+            result.push_back(nums[0][0]);
+            result.push_back(nums[0][0]);
+            return result;
+        }
 
-		set<tuple<int, int, int>> ranges;
-		for (int i = 0; i < nums.size(); i++) {
-			ranges.insert(make_tuple(nums[i][0], 0, i));
-		}
-		int L = 0, R = -1;
-		while (true) {
-			tuple<int, int, int> currentMin = *ranges.begin();
-			tuple<int, int, int> currentMax = *ranges.rbegin();
+        set<tuple<int, int, int>> ranges;
+        for (int i = 0; i < nums.size(); i++) {
+            ranges.insert(make_tuple(nums[i][0], 0, i));
+        }
+        int L = 0, R = -1;
+        while (true) {
+            tuple<int, int, int> currentMin = *ranges.begin();
+            tuple<int, int, int> currentMax = *ranges.rbegin();
 
-			int l = get<0>(currentMin);
-			int r = get<0>(currentMax);
+            int l = get<0>(currentMin);
+            int r = get<0>(currentMax);
 
-			int currentMinRange = R - L;
-			int currentRange = r - l;
+            int currentMinRange = R - L;
+            int currentRange = r - l;
 
-			if (currentMinRange < 0 || currentRange < currentMinRange) {
-				L = l;
-				R = r;
-			}
+            if (currentMinRange < 0 || currentRange < currentMinRange) {
+                L = l;
+                R = r;
+            }
 
-			int index = get<1>(currentMin);
-			int arrayIndex = get<2>(currentMin);
+            int index = get<1>(currentMin);
+            int arrayIndex = get<2>(currentMin);
 
-			if (index == nums[arrayIndex].size() - 1)
-				break;
+            if (index == nums[arrayIndex].size() - 1)
+                break;
             ranges.erase(currentMin);
-			ranges.insert(make_tuple(nums[arrayIndex][index + 1], index + 1, arrayIndex));
-		}
+            ranges.insert(make_tuple(nums[arrayIndex][index + 1], index + 1, arrayIndex));
+        }
 
-		result.push_back(L);
-		result.push_back(R);
-		return result;
-	}
+        result.push_back(L);
+        result.push_back(R);
+        return result;
+    }
 };
