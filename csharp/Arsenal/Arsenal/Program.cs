c46dbbc7d5ba77b33d9e95f73d6c902d9643d444
WangXI <867110907@qq.com>
2018-04-19 21:28:21 +0800

    change concurrent file reader to a more general concurrent executer

--- csharp/Arsenal/Arsenal/Program.cs
+++ csharp/Arsenal/Arsenal/Program.cs
@@ -1,20 +1,10 @@
-﻿using MultiThread;
-using System.Collections.Generic;
-using System.Collections.Concurrent;
-namespace Arsenal
+﻿namespace Arsenal
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ConcurrentQueue<int> length = new ConcurrentQueue<int>();
-            ConcurrentFileReader reader = new ConcurrentFileReader(@"D:\Wang\test.txt", str => { length.Enqueue(str.Length); });
-            System.Console.WriteLine(reader.ReadLines());
-
-            foreach (int l in length)
-            {
-                System.Console.WriteLine(l);
-            }
+            
         }
     }
 }
