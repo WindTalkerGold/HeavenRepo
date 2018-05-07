a26488e66db6cc608e4587b4e017ad7795cec11e
WindTalkerGold <wang.xiw@outlook.com>
2018-05-07 23:21:24 +0800

    throttling and demo (#14)
    * throttling and demo
    
    * uncomment thread yield

---
+++ csharp/Arsenal/MultiThread/Throttle/IThrottleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace MultiThread.Throttle
+{
+    public interface IThrottleController
+    {
+        /// <summary>
+        /// Queue a process to be processed
+        /// </summary>
+        /// <returns>true if queued successfully, false if exceeded the limitation</returns>
+        bool QueueProcess(IThrottableProcess process);
+
+        /// <summary>
+        /// Start allocating quotas
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Stop allocating quotas and dispose after all queued processes finishes
+        /// </summary>
+        void Shutdown();
+    }
+}
