0ad251438b928ff273ced5bde8e4151c93511492
867110907@qq.com <867110907@qq.com>
2018-09-22 08:34:25 +0800

    service bus connector

---
+++ csharp/Azure/HeavenGo/HeavenGo.V2/Controllers/ServiceBusQueueController.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+
+
+namespace HeavenGo.V2.Controllers
+{
+    using ServiceBusConnector;
+
+    public class ServiceBusQueueController : Controller
+    {
+        // GET api/<controller>
+        public string Get()
+        {
+            return "Test";
+        }
+        
+        [System.Web.Mvc.HttpPost]
+        public async Task<string> Post(string queueName, [FromBody] string message)
+        {
+            ServiceBusConnector connector = new ServiceBusConnector("", queueName);
+            await connector.SendMessage(message);
+            return "queueName";
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ csharp/Azure/HeavenGo/ServiceBusConnector/ServiceBusConnector.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace ServiceBusConnector
+{
+    public class ServiceBusConnector
+    {
+        public string QueueName { get; }
+        public string ConnectionString { get; }
+        private readonly IQueueClient queueClient;
+
+        public ServiceBusConnector(string connectString, string queueName)
+        : this(connectString, queueName, new QueueClient(connectString, queueName))
+        {
+        }
+
+        public ServiceBusConnector(string connectString, string queueName, IQueueClient queueClient)
+        {
+            this.ConnectionString = connectString;
+            this.QueueName = queueName;
+            this.queueClient = queueClient;
+        }
+
+        public async Task SendMessage(string message)
+        {
+            Message msg = new Message(Encoding.UTF8.GetBytes(message));
+            await queueClient.SendAsync(msg);
+        }
+    }
+}
