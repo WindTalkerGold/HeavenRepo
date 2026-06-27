c4a8483b5d288d3dc9b8533d04ba2c29e93ce747
WangXI <wang.xiw@outlook.com>
2026-06-27 16:28:52 +0800

    chore: repurpose repo as agentic playground
    - Move Scripts/csharp/learnings/leetcode/stock out to ../backup (kept off-tree)
    - Keep only site/ + .github/ (Azure Static Web Apps)
    - Rewrite README and .gitignore for the new static-site playground
    - Add typing practice app under site/typing/ and link from site/index.html

--- csharp/Azure/HeavenGo/HeavenGo.V2/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HeavenGo.V2.MvcApplication" Language="C#" %>
---
+++ site/typing/data/letters.js
@@ -0,0 +1,6 @@
+window.TYPING_DATA = window.TYPING_DATA || {};
+window.TYPING_DATA.letters = {
+  "letters": "abcdefghijklmnopqrstuvwxyz",
+  "symbolsNoShift": ",.;'/[]-=",
+  "symbolsShift": "!@#$%^&*()_+?\""
+}
